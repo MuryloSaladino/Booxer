b4d4af87092d176cd589d389c770154e3a567c0d
MuryloSaladino <murylosaladino@gmail.com>
2025-06-25 00:09:36 -0300

    feat: identity dependency injection config

---
+++ Booxer.Infrastructure/Identity/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Booxer.Domain.Identity;
+using Booxer.Infrastructure.Identity.Context;
+using Booxer.Infrastructure.Identity.Services;
+
+namespace Booxer.Infrastructure.Identity;
+
+public static class IdentityConfiguration
+{
+    public static void ConfigureIdentity(this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordEncrypter, PasswordEncrypter>();
+        services.AddScoped<ITokenAuthenticator, TokenAuthenticator>();
+        
+        services.AddScoped<ISessionContext, SessionContext>();
+    }
+}
