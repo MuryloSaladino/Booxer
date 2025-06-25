1a93c3b6ea0d3af78bba1d3298cc904ab3ed8c2a
MuryloSaladino <murylosaladino@gmail.com>
2025-06-25 00:06:49 -0300

    feat: application dependecy injection config

---
+++ Booxer.Application/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Booxer.Application.Pipeline.Authentication;
+using Booxer.Application.Pipeline.Logging;
+using Booxer.Application.Pipeline.Validation;
+
+namespace Booxer.Application;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthenticationBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+    }
+}
