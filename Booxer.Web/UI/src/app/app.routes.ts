d54cdbc3ec72b5eabff4e5e996ee5b7ebf5d72a6
MuryloSaladino <murylosaladino@gmail.com>
2025-06-27 16:04:19 -0300

    feat: web ui root and routes config

---
+++ Booxer.Web/UI/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { AppRoutes } from './core/constants/app-routes';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    {
+		path: "",
+		redirectTo: AppRoutes.LOGIN,
+		pathMatch: 'full',
+	},
+	{
+		path: AppRoutes.NOT_FOUND,
+		component: NotFoundComponent,
+	},
+	{
+		path: AppRoutes.LOGIN,
+		component: LoginComponent,
+	},
+    {
+		path: "",
+		canActivate: [authGuard],
+		children: [
+			{
+				path: AppRoutes.DASHBOARD,
+                component: DashboardComponent,
+			},
+		]
+	},
+	{
+		path: "**",
+		redirectTo: AppRoutes.NOT_FOUND,
+	},
+];
--- Booxer.WebAPI/Constants/Cookies.cs
+++
@@ -1,7 +0,0 @@
-namespace Booxer.WebAPI.Constants;
-
-public static class Cookies
-{
-    public const string AccessToken = "accessToken";
-    public const string RefreshToken = "refreshToken";
-}
