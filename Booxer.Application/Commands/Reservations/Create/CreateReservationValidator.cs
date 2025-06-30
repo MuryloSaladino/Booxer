d7ba31765f75d5416488d84b260aee12e69465f6
MuryloSaladino <murylosaladino@gmail.com>
2025-06-30 02:48:14 -0300

    feat: messages for reservation validation errors

--- Booxer.Application/Commands/Reservations/Create/CreateReservationValidator.cs
+++ Booxer.Application/Commands/Reservations/Create/CreateReservationValidator.cs
@@ -10,9 +10,12 @@ public class CreateReservationValidator : AbstractValidator<CreateReservationReq
     {
         RuleFor(r => r.StartsAt)
             .Must(d => d > DateTime.Now)
-            .Must(d => d.Hour > 8 && d.Hour < 18);
+            .Must(d => d.Hour >= 8 && d.Hour <= 18)
+            .WithMessage("Start date invalid, must be in the future and between 08:00 and 18:00");
         RuleFor(r => r.EndsAt)
-            .Must(d => d.Hour > 8 && d.Hour < 18);
+            .Must(d => d > DateTime.Now)
+            .Must(d => d.Hour >= 8 && d.Hour <= 18)
+            .WithMessage("End date invalid, must be in the future and between 08:00 and 18:00");
 
         RuleFor(r => r)
             .Must(r => r.EndsAt > r.StartsAt)
@@ -21,12 +24,16 @@ public class CreateReservationValidator : AbstractValidator<CreateReservationReq
                 var duration = r.EndsAt - r.StartsAt;
                 return duration <= new TimeSpan(8, 0, 0) && duration > TimeSpan.Zero;
             })
+            .WithMessage("Invalid range, must be 8 hours maximum");
+        
+        RuleFor(r => r)
             .MustAsync(async (r, cancellationToken) => !await reservationsRepository
                 .Exists(new()
                 {
                     ResourceId = r.ResourceId,
                     Start = r.StartsAt,
                     End = r.EndsAt,
-                }, cancellationToken));
+                }, cancellationToken))
+            .WithMessage("There is already a reservation in that range");
     }
 }
