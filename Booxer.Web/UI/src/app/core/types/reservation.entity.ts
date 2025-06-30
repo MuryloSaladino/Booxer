74d12961759b7cde6c4fb2befc47369c5b66ddbe
MuryloSaladino <murylosaladino@gmail.com>
2025-06-30 00:04:07 -0300

    fix: category property in reservation resource

--- Booxer.Web/UI/src/app/core/types/reservation.entity.ts
+++ Booxer.Web/UI/src/app/core/types/reservation.entity.ts
@@ -1,10 +1,11 @@
 import { BaseEntity } from "./base.entity";
+import { Category } from "./category.entity";
 import { Resource } from "./resource.entity";
 
 export interface Reservation extends BaseEntity {
     startsAt: Date;
     endsAt: Date;
-    resource: Resource;
+    resource: Resource & { category: Category };
     reservedBy: string;
 }
 
