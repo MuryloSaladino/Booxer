df7781870094decc124d56c70746fa3dada922f3
MuryloSaladino <murylosaladino@gmail.com>
2025-06-29 23:57:44 -0300

    feat: time rules for reservations

--- Booxer.Application/Commands/Reservations/Create/CreateReservationValidator.cs
+++ Booxer.Application/Commands/Reservations/Create/CreateReservationValidator.cs
@@ -8,8 +8,19 @@ public class CreateReservationValidator : AbstractValidator<CreateReservationReq
     public CreateReservationValidator(
         IReservationsRepository reservationsRepository)
     {
+        RuleFor(r => r.StartsAt)
+            .Must(d => d > DateTime.Now)
+            .Must(d => d.Hour > 8 && d.Hour < 18);
+        RuleFor(r => r.EndsAt)
+            .Must(d => d.Hour > 8 && d.Hour < 18);
+
         RuleFor(r => r)
             .Must(r => r.EndsAt > r.StartsAt)
+            .Must(r =>
+            {
+                var duration = r.EndsAt - r.StartsAt;
+                return duration <= new TimeSpan(8, 0, 0) && duration > TimeSpan.Zero;
+            })
             .MustAsync(async (r, cancellationToken) => !await reservationsRepository
                 .Exists(new()
                 {
